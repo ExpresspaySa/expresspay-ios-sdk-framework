71fadb8e88d5e7609f726d91b5e2186ab655975c
Zohaib <zohaib.kambrani@edfapay.com>
2023-02-06 12:01:48 +0300

    no message

---
+++ Sample/Credentials.swift
@@ -0,0 +1,12 @@
+//
+//  Credentials.swift
+//  Sample
+//
+//  Created by Zohaib Kambrani on 06/02/2023.
+//
+
+import Foundation
+
+var merchantKey = "<merchant key>";
+var merchantPassword = "<merchant password>";
+var merchantIdentifier = "merchant.com.expresspay.sdk";
